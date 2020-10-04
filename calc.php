cd3c27811329d5b6bdac038c7f6aade75f126963
kcotton1 <70120353+kcotton1@users.noreply.github.com>
2020-10-04 17:17:22 -0400

    Update calc.php

--- calc.php
+++ calc.php
@@ -45,7 +45,7 @@
             <tr>
                <td><input type="button" name="clear" value="c" onclick="calc.display.value = ''"></td>
                <td><input type="button" name="0" value="0" onclick="calc.display.value += '0'"></td>
-		<td><input type="button" name="enter" value="=" onclick="calc.display.value = try: eval(calc.display.value) except SyntaxError: Invalid Value"></td>
+		<td> try: <input type="button" name="enter" value="=" onclick="calc.display.value = eval(calc.display.value)"> except SyntaxError: calc.display.value='Invalid Value'</td>
                <td><input type="button" class="operator" name="div" value="/" onclick="calc.display.value += '/'"></td>
             </tr>
          </table>
