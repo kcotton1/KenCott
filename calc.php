6fb2e3e45f8e7ecc444c01621e497429fc2af876
kcotton1 <70120353+kcotton1@users.noreply.github.com>
2020-10-04 16:33:37 -0400

    Update calc.php

--- calc.php
+++ calc.php
@@ -45,10 +45,12 @@
             <tr>
                <td><input type="button" name="clear" value="c" onclick="calc.display.value = ''"></td>
                <td><input type="button" name="0" value="0" onclick="calc.display.value += '0'"></td>
-		    try:
-               <td><input type="button" name="enter" value="=" onclick="calc.display.value = eval(calc.display.value)"></td>
+		   
+               <td><input type="button" name="enter" value="=" 
+			   try:
+			  onclick="calc.display.value = eval(calc.display.value)"></td>
 		    except:
-		    	<td><input type="button" name="enter" value="=" onclick="calc.display.value = 'Error invalid value'"></td>
+		    	onclick="calc.display.value = 'Error invalid value'"></td>
                <td><input type="button" class="operator" name="div" value="/" onclick="calc.display.value += '/'"></td>
             </tr>
          </table>
