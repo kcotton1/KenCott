64557036711d368ba5e2b78cc0c6c2eacbb8af39
kcotton1 <70120353+kcotton1@users.noreply.github.com>
2020-10-04 17:23:34 -0400

    Update calc.php

--- calc.php
+++ calc.php
@@ -4,6 +4,10 @@
  <meta name="Kenji Cotton" content="calculator, basic math, it5236">
  <title>Calculator</title>
  <link rel="stylesheet" href="css/calccolor.css">
+	def calculate(x):
+		try:
+		   eval(x)
+		except: 'Invalid Error'
    <body>
       <form name="calc">
          <table>
@@ -45,7 +49,7 @@
             <tr>
                <td><input type="button" name="clear" value="c" onclick="calc.display.value = ''"></td>
                <td><input type="button" name="0" value="0" onclick="calc.display.value += '0'"></td>
-		try: <td><input type="button" name="enter" value="=" onclick="calc.display.value = eval(calc.display.value)"></td> except SyntaxError: calc.display.value='Invalid Value'
+		<td><input type="button" name="enter" value="=" onclick="calc.display.value = calculate(calc.display.value)"></td>
                <td><input type="button" class="operator" name="div" value="/" onclick="calc.display.value += '/'"></td>
             </tr>
          </table>
